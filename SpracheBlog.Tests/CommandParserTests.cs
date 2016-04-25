13faf2eb0fc16e1538c3eb191920905edea1d4d5
Jeremy Davis <djdavis@iee.org>
2016-04-25 08:30:34 +0100

    Initial code from experimentation

---
+++ SpracheBlog.Tests/CommandParserTests.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SpracheBlog;
+using Sprache;
+using System.Linq;
+
+namespace SpracheBlog.Tests
+{
+
+    [TestClass]
+    public class CommandParserTests
+    {
+        [TestMethod]
+        public void FieldParses()
+        {
+            var result = Command.Field.TryParse("alpha=\"one\"");
+
+            Assert.IsTrue(result.WasSuccessful, result.Message);
+        }
+
+        [TestMethod]
+        public void CreateParses()
+        {
+            var result = Command.CreateCommand.TryParse("create {123} under /123/234 with alpha=\"one\", beta=\"two\"");
+
+            Assert.IsTrue(result.WasSuccessful, result.Message);
+
+            var cmd = result.Value as CreateCommand;
+
+            Assert.AreEqual("/123/234", cmd.Location);
+            Assert.AreEqual("{123}", cmd.Template);
+            Assert.AreEqual(2, cmd.Fields.Count());
+            Assert.AreEqual("alpha", cmd.Fields.First().Name);
+        }
+
+        [TestMethod]
+        public void MoveParses()
+        {
+            var result = Command.MoveCommand.TryParse(@"move \abc\def to {456}");
+
+            Assert.IsTrue(result.WasSuccessful, result.Message);
+
+            var cmd = result.Value as MoveCommand;
+
+            Assert.AreEqual("\\abc\\def", cmd.Item);
+            Assert.AreEqual("{456}", cmd.NewLocation);
+        }
+
+        [TestMethod]
+        public void BadMoveDoesNotParse()
+        {
+            var result = Command.MoveCommand.TryParse(@"move \abc\def ao {456}");
+
+            Assert.IsFalse(result.WasSuccessful);
+        }
+
+        [TestMethod]
+        public void DeleteParses()
+        {
+            var result = Command.DeleteCommand.TryParse(@"delete \abc\def\x");
+
+            Assert.IsTrue(result.WasSuccessful, result.Message);
+
+            var cmd = result.Value as DeleteCommand;
+
+            Assert.AreEqual("\\abc\\def\\x", cmd.Item);
+        }
+
+        [TestMethod]
+        public void AnyParsesCreate()
+        {
+            var result = Command.Any.TryParse("create \\12 under {xyz} with a=\"2\"");
+
+            Assert.IsTrue(result.WasSuccessful, result.Message);
+            Assert.IsInstanceOfType(result.Value, typeof(CreateCommand));
+
+            var cmd = result.Value as CreateCommand;
+
+            Assert.AreEqual("\\12", cmd.Template);
+            Assert.AreEqual("{xyz}", cmd.Location);
+            Assert.AreEqual(1, cmd.Fields.Count());
+            Assert.AreEqual("a", cmd.Fields.First().Name);
+        }
+    }
+
+}
---
+++ SpracheBlog/Command.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Sprache;
+
+namespace SpracheBlog
+{
+
+    public class Field
+    {
+        public string Name { get; set; }
+        public string Value { get; set; }
+    }
+
+    public class CreateCommand : Command
+    {
+        public string Template { get; set; }
+        public string Location { get; set; }
+        public IEnumerable<Field> Fields { get; set; }
+    }
+
+    public class MoveCommand : Command
+    {
+        public string Item { get; set; }
+        public string NewLocation { get; set; }
+    }
+
+    public class DeleteCommand : Command
+    {
+        public string Item { get; set; }
+    }
+
+    public class Command
+    {
+        public static Parser<Field> Field =
+            from name in Parse.AnyChar.Until(Parse.Char('=')).Text()
+            from openQuote in Parse.Char('"')
+            from value in Parse.AnyChar.Until(Parse.Char('"')).Text()
+            select new Field() { Name = name, Value = value };
+
+        private static IEnumerable<T> Concatenate<T>(T first, IEnumerable<T> rest)
+        {
+            yield return first;
+            foreach(T item in rest)
+            {
+                yield return item;
+            }
+        }
+
+        // create <template:id/path> under <location:id/path> with <property=value>[,etc]
+        public static Parser<Command> CreateCommand =
+            from cmd in Parse.IgnoreCase("create").Token()
+            from template in Parse.AnyChar.Until(Parse.WhiteSpace).Text().Token()
+            from under in Parse.IgnoreCase("under").Token()
+            from location in Parse.AnyChar.Until(Parse.WhiteSpace).Text().Token()
+            from with in Parse.IgnoreCase("with").Token()
+            from fields in ( 
+                from first in Field
+                from rest in Parse.Char(',').Then(_ => Field).Many()
+                select Concatenate(first, rest) )
+            select new CreateCommand() { Template = template, Location = location, Fields = fields };
+
+        // move <item:id/path> to <location:id/path>
+        public static Parser<Command> MoveCommand =
+            from cmd in Parse.IgnoreCase("move").Token()
+            from item in Parse.AnyChar.Until(Parse.WhiteSpace).Text().Token()
+            from to in Parse.IgnoreCase("to").Token()
+            from newLocation in Parse.AnyChar.Many().Text()
+            select new MoveCommand() { Item = item, NewLocation = newLocation };
+
+        //delete <item:id/path>
+        public static Parser<Command> DeleteCommand =
+            from cmd in Parse.IgnoreCase("delete").Token()
+            from item in Parse.AnyChar.Many().Text()
+            select new DeleteCommand() { Item = item };
+
+        public static Parser<Command> Any =
+            CreateCommand
+            .XOr(MoveCommand)
+            .XOr(DeleteCommand);
+    }
+
+}
